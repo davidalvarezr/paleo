f85b9e57418bdf1314145c55aabdcf1245be957d
ayrton <ayrtondumas@gmail.com>
2019-05-17 17:11:25 +0200

    code decrypt

--- code/client/main.js
+++ code/client/main.js
@@ -3,13 +3,16 @@ cocktail_incr = 0
 points = 0
 
 function generateQRCODE( id_cocktail, price ) {
-    cocktail_incr++;
-    $("#qrcode").empty()
-    new QRCode(document.getElementById("qrcode"), id_cocktail + "," + cocktail_incr)
-    $('#modal1').openModal()
-    points -= price
-    setPoints(points)
-    refreshAvailability()
+    
+    if (price <= points) {
+        cocktail_incr++;
+        $("#qrcode").empty()
+        new QRCode(document.getElementById("qrcode"), id_cocktail + "," + cocktail_incr)
+        $('#modal1').openModal()
+        points -= price
+        setPoints(points)
+        refreshAvailability()
+    }
 }
 
 function validateCode() {
--- code/server/routes/code.js
+++ code/server/routes/code.js
@@ -8,7 +8,23 @@ router.get('/', function(req, res, next) {
 
 router.get('/validate/:id', function(req, res, next) {
     //process
-  res.json({'code': 'success', 'points':req.params.id});
+    console.log("test")
+  points = parseInt(decypher(req.params.id))
+  console.log("points= ", points)
+  res.json({'code': 'success', 'points': points});
 });
+
+
+function decypher(code) {
+  var points = "";
+  for (var i = 0; i < code.length; i+=2) {
+    var one = code.charAt(i).charCodeAt(0);
+    var two = code.charAt(i+1).charCodeAt(0);
+    points += "" + Math.abs(one-two);
+  }
+  return points
+}
+
+
 
 module.exports = router;
