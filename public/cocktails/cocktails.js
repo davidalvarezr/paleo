294ba6131205cf23130134b9ccc789a3c1329172
David Alvarez <david.alvarezrestrepo@edu.hefr.ch>
2019-07-16 12:14:43 +0200

    Added cocktails in HTML, have to test it

---
+++ public/cocktails/cocktails.js
@@ -0,0 +1,39 @@
+const cocktails = [
+    {
+        id: 1,
+        name: 'The complexity',
+        pointsRequired: 60,
+        composition: 'Pêche, fleurs de sureau, thé vert et petits fruits',
+        img: null,
+    },
+    {
+        id: 2,
+        name: 'Tropical Island',
+        pointsRequired: 40,
+        composition: 'Thé vert et petits fruits',
+        img: null,
+    },
+    {
+        id: 3,
+        name: 'Temptation reef',
+        pointsRequired: 19,
+        composition: 'Fleurs de sureau et petits fruits',
+        img: null,
+    },
+    {
+        id: 4,
+        name: 'Banshee',
+        pointsRequired: 20,
+        composition: 'Pêche ert fleurs de sureau',
+        img: null,
+    },
+    {
+        id: 5,
+        name: 'Thé sur ?',
+        pointsRequired: 24,
+        composition: 'Fleurs de sureau et thé vert',
+        img: null,
+    },
+]
+
+export default cocktails;
--- public/main.js
+++ public/main.js
@@ -1,9 +1,12 @@
+import cocktails from './cocktails';
+
 
 cocktail_incr = 0
 points = getCookie("points") == "" ? 0 : parseInt(getCookie("points"))
 
 function init() {
     setPoints(points)
+    printCocktails(cocktails);
 }
 
 function generateQRCODE( id_cocktail, price ) {
@@ -68,4 +71,24 @@ function getCookie(cname) {
         }
     }
     return "";
+}
+
+function printCocktails(cocktails) {
+    cocktails.forEach(cocktail => {
+        var cocktailHTML = 
+            `<div class="card cocktail-card col s12 l3 locked" price="100">
+                <div class="card-image">
+                <img src="https://www.jamieoliver.com/drinks-tube/wp-content/uploads/2014/06/Mojito.jpg">
+                <span class="card-title">${cocktail.name}</span>
+                </div>
+                <div class="card-content">
+                <p>${cocktail.composition}</p>
+                </div>
+                <div class="card-action">
+                <a onclick="${generateQRCODE(cocktail.id, cocktail.pointsRequired)}">Commander (${cockatail.pointsRequired})</a>
+                </div>
+            </div>`;
+
+        $("cocktail-gallery").append(cocktailHTML);
+    });
 }
